c6e292a901f3986e5b0a65c21f540ce45c71a9f1
Guilherme Valença Saunders <guilhermevalenca.s7@gmail.com>
2025-10-16 09:31:28 -0300

    Modificações na Classe Movie

--- src/Product/Movie.cpp
+++ src/Product/Movie.cpp
@@ -11,29 +11,35 @@
         void setMainActors(string mainActors);
         const string& getMainActors() const;*/
     
-    Movie :: Movie(
-        string name, string description,
-        string director, string mainActors,
-        string ownersConsiderations, string owner,
-        string genre, string idiom, int rentValue,
-        string recommendedAge, int id
-    ){  
-        
-        setName(name);
-        setDescription(description);
-        setDirector(director); // not definided 
-        setMainActors(mainActors); // not definided 
-        setOwnersConsiderations(ownersConsiderations);
-        setOwner(owner);
-        setGenre(genre);
-        setIdiom(idiom);
-        setRentValue(rentValue);
-        setRecommendedAge(recommendedAge);
-        setId(id);
+        Movie :: Movie(
+            string name, string description,
+            string director, string mainActors,
+            string ownersConsiderations, string owner,
+            string genre, string idiom, int rentValue,
+            string recommendedAge, int id
+        ){  
+            
+            setName(name);
+            setDescription(description);
+            setDirector(director); // not definided 
+            setMainActors(mainActors); // not definided 
+            setOwnersConsiderations(ownersConsiderations);
+            setOwner(owner);
+            setGenre(genre);
+            setIdiom(idiom);
+            setRentValue(rentValue);
+            setRecommendedAge(recommendedAge);
+            setId(id);
 
-        cout << "Your book has been added to the system. Now other users can access it." << endl; 
-    }
+            cout << "Your movie has been added to the system. Now other users can access it." << endl; 
+        }
 
-    Movie :: ~Movie(){
-        
-    }
+        Movie :: ~Movie(){
+            
+        }
+
+        void Movie :: setDirector(string director) {this->director = move(director);}
+        const string& Movie :: getDirector() const {return director;}
+
+        void Movie :: setMainActors(string mainActor) {this->mainActors = move(mainActors);}
+        const string& Movie :: getMainActors() const {return mainActors;}
