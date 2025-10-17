a767f02685f13c5650508f24d93f3e40b090012a
danielgacioly <dgla@cin.ufpe.br>
2025-10-17 11:03:28 -0300

    ajuste de conflitos

--- src/Customer/user.cpp
+++ src/Customer/user.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 User::User(const string& i_email,
            const string& i_cpf,
-           const string& i_fullname)
+           const string& i_fullname, const string& i_birthday)
     : cpf(i_cpf),
       fullname(i_fullname),
-      email(i_email) {}
+      email(i_email),
+      birthday(i_birthday) {}
 
 // Update CPF or full name based on the 'update' code (UPDATE)
-int User::update_info(const string& new_cpf, const string& new_fullname, int update){
+int User::update_info(const string& new_cpf, const string& new_fullname, const string& new_birthday, int update){
     if(update == 0){
         return 0; // No update
     } else if(update == 1){
@@ -18,6 +19,9 @@ int User::update_info(const string& new_cpf, const string& new_fullname, int upd
     } else if(update == 2){
         fullname = new_fullname; // Update full name
         return 2;
+    } else if(update == 3){
+        birthday = new_birthday; // Update full name
+        return 3;
     } else{
         return update; // Invalid update code, return as it is
     }
--- src/Customer/user.h
+++ src/Customer/user.h
@@ -14,17 +14,18 @@ public:
     // Constructor - CREATE
     User(const string& i_email,
             const string& i_cpf,
-            const string& i_fullname);
+            const string& i_fullname, const string& i_birthday);
     
     // Getters - READ
     string getCpf() const { return cpf; };
     string getFullname() const { return fullname; };
     string getEmail() const { return email; };
+    string getBirdthay() const { return birthday; };
 
     //cod de daniel
 
     //COLOCAR BIRTHDAY!!!!!!!!!!!!!! 
-    int update_info(const string& new_cpf, const string& new_fullname, int update); // Update CPF or full name
+    int update_info(const string& new_cpf, const string& new_fullname, const string& new_birthday, int update); // Update CPF or full name
 
 };
 
