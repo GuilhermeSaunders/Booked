6c81c5d3b22604701a2d8e6ead0b6b337c0d5c4c
Lucas Mendonça <lucascmendoncaa@gmail.com>
2025-10-23 19:45:51 -0300

    validate

--- src/BusinessRules/validate.cpp
+++ src/BusinessRules/validate.cpp
@@ -1,33 +1,123 @@
 #include "validate.h"
+#include "user.h"
+#include "account.h"
 #include <functional>
 #include <string>
+#include <cctype>
+#include <random>
 
 using namespace std;
 
 
 
-size_t Authentication::hashPassword(const string& password) {
+size_t Validate::hashPassword(const string& password) {
     hash<string> hasher;
     return hasher(password); 
 }
 
-bool Authentication::createUser(string& username, string& password, string& email, string& cpf, string& birthday) {
-    if (username.length() < 3 || username.length() > 15) { return false; } ;
+string Validate::generateId(int length = 6) { // generates a random ID
+    string id;
+    mt19937 rng(std::random_device{}());
+    uniform_int_distribution<int> dist(0, 9);
 
-    if ((email.find('@') == string::npos) && (email.find('.com') == string::npos)) { return false; };
+    for (int i = 0; i < length; ++i)
+        id += std::to_string(dist(rng));
 
-    if (password.length() <= 4) { return false; };
-
-    if (cpf.length() != 11) { return false; };
-
-    if ((birthday.length() != 10) && (birthday[2] != '/' || birthday[5] != '/')) { return false; };
-
-    this->username = username;
-    this->passwordHash = hashPassword(password);
-    this->email = email;
-    this->cpf = cpf;
-    this->birthday = birthday;
-
-    return true;
+    return id;
 }
 
+bool Validate::validateUsername(const string& username) {
+    if (username.length() < 3 || username.length() > 15) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool Validate::validateStringPassword(const string& password) {
+    if (password.length() < 6) {
+        return false;
+    } 
+
+    bool hasUpper = false;   
+    bool hasLower = false;   
+    bool hasDigit = false;    
+    bool hasSpecial = false;  
+
+    for (char ch : password) {
+        if (isupper(ch)) {
+            hasUpper = true;
+        } if (islower(ch)) {
+            hasLower = true;
+        } if (isdigit(ch)) {
+            hasDigit = true;
+        } if (ispunct(ch)) {
+            hasSpecial = true;
+        }
+    }
+    
+    return hasUpper && hasLower && hasDigit && hasSpecial;
+}
+
+bool Validate::validateEmail(const string& email) {
+    if ((email.find('@') == string::npos) && (email.find(".com") == string::npos)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool Validate::validateCpf(const string& cpf) {
+    if (cpf.length() != 11) { 
+        return false; 
+    } else {
+        return true;
+    }
+}
+
+bool Validate::validateBirthday(const string& birthday) {
+    if ((birthday.length() != 10) && (birthday[2] != '/' || birthday[5] != '/')) {
+        return false;
+    }
+
+    for (char ch : birthday) {
+        if (!isdigit(ch)) {
+            return false;
+        }
+    }
+    
+}
+
+bool Validate::validateFullname(const string& fullname) {
+
+    if (fullname.length() < 5) {
+        return false;
+    } if (fullname.front() == ' ' || fullname.back() == ' ') {
+        return false;
+    }
+
+    for (char ch : fullname) {
+        
+        if (std::isdigit(ch)) {
+            return false;
+        } if (std::ispunct(ch)) {
+
+            if (ch != '-' && ch != '\'') {
+                return false;
+            }
+        }
+    }
+}
+
+User Validate::createUser(const string& email, const string& cpf, const string& fullname, const string& birthday) {
+
+
+    return User(email, cpf, fullname, birthday);
+}
+
+Account Validate::createAccount(const string& stringPassword, const string& username, const User& user) {
+    size_t hash = hashPassword(stringPassword);
+    string id = generateId();
+
+    return Account(id, hash, username, user);
+}
--- src/BusinessRules/validate.h
+++ src/BusinessRules/validate.h
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class Authentication {
+class Validate {
 
     private:
 
@@ -10,15 +10,23 @@ class Authentication {
     size_t passwordHash; // Stores the hashed representation of the user's password for secure authentication
     string email; 
     string cpf;
-    string birthday; 
+    string birthday;
+    string fullname;
     
-    size_t hashPassword(const string& password); 
+    size_t hashPassword(const string& password);
+    string generateId(const int lenght = 6);
 
     public:
 
+    bool validateUsername(const string& username);
+    bool validateStringPassword(const string& password);
+    bool validateEmail(const string& email);
+    bool validateCpf(const string& cpf);
+    bool validateBirthday(const string& birthday);
+    bool validateFullname(const string& fullname);
 
-
-    bool createUser(string& username, string& password, string& email, string& cpf, string& birthday);
+    User createUser(const string& email, const string& cpf, const string& fullname, const string& birthday);
+    Account createAccount(const string& stringPassword, const string& username, const User& user);
  
     // getters
 
