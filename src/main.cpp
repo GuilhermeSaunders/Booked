8e1e220fcba4e3600df150eca36ee3b61483a7e3
danielgacioly <dgla@cin.ufpe.br>
2025-10-30 21:26:51 -0300

    includes no main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 //main no terminal e essas merdas
 
-
-
 //função do Hub - chama outras funções para cada respectiva página
 //funções das páginas
 
 //uma espécie de "recursão" entre funções
+
+#include <iostream>
+#include "Customer/user.h"
+#include "Customer/account.h"
+
+#include "BusinessRules/login.h"
+#include "BusinessRules/validate.h"
+#include "BusinessRules/rental.h"
+
+#include "Product/Product.h"
+#include "Product/Book.h"
+#include "Product/Movie.h"
+#include "Product/board_game.h"
+#include "Product/video_game.h"
+
+using namespace std;
